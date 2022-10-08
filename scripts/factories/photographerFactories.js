12ade15073cf9dc6c1d681f17097418093205c3d
LixECode <101730639+LixECode@users.noreply.github.com>
2022-10-08 16:08:11 +0200

    MAJ FILES

--- scripts/factories/photographerFactories.js
+++ scripts/factories/photographerFactories.js
@@ -1,17 +1,24 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const {
+        name,
+        portrait
+    } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {
+        name,
+        picture,
+        getUserCardDOM
+    }
 }
